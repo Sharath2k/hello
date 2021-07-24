5d8b060ef43930255535a3fb3574d0ac86d9bb66
Sharath <Sharath@email.com>
2021-07-24 11:57:18 +0800

    B updated the code

--- file.py
+++ file.py
@@ -1,5 +1,6 @@
 print('Python project started')
+print('new code added by B')
 x = 10
 y = 20
 res = x-y if x>y else y-x
-print(f'Python code results: {res}')
+print(f'final output code results: {res}')
